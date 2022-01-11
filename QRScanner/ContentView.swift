03d407ec9d31cb40e4e3b536858f2c9a9cde36f7
atmeed <leo.zemtsoff@gmail.com>
2022-01-11 18:09:21 +0300

    Create Base QR Scanner

--- QRScanner/ContentView.swift
+++ QRScanner/ContentView.swift
@@ -6,11 +6,38 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan QR code to get started"
+    
+    var scannedSheet: some View {
+        CodeScannerView (
+            codeTypes: [.qr],
+            completion:  { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+                
+            }
+        )
+            
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            Button("Scan Qr") {
+                self.isPresentingScanner = true
+            }
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannedSheet
+            }
+        }
     }
 }
 
