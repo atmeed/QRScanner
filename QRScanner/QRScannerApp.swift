66825cc1e79a0de5baea0cd7ad29ff9b3094dcc5
atmeed <leo.zemtsoff@gmail.com>
2022-01-11 17:52:56 +0300

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Leonid Zemtsov on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
